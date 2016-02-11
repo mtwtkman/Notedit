c34c53b93b2521f52625866e4b453e451b92e341
mtwtkman <punipuniomochi@gmail.com>
2016-02-11 23:36:29 +0900

    add bin

---
+++ bin/cmd.js
@@ -0,0 +1,18 @@
+'use strict';
+var spawn = require('child_process').spawn;
+var electron = require('electron-prebuilt');
+var path = require('path');
+
+var detached = process.argv.indexOf('--detach') != -1;
+var args = [path.join(__dirname, '..')];
+
+if (detached) {
+  spawn(electron, args, {
+    stdio: 'ignore',
+    detached: true
+  }).unref();
+} else {
+  spawn(electron, args, {
+    stdio: 'inherit'
+  });
+}
